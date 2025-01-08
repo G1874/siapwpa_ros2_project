8bea2913c63993545b5e7c196991e112514f21fe
Grzegorz N <gniedziolka@student.agh.edu.pl>
2025-01-08 15:10:54 +0000

    Added a launch file

--- autonomous_vehicle/setup.py
+++ autonomous_vehicle/setup.py
@@ -1,15 +1,17 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'autonomous_vehicle'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
